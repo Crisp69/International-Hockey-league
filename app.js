38f895a170a659b115e5a54fb903646c6bd76b06
Crisp69 <64032061+Crisp69@users.noreply.github.com>
2020-07-19 12:46:46 +0200

    ss

---
+++ app.js
@@ -0,0 +1,94 @@
+
+var img = document.createElement("img");
+img.src = "img/tp-logo.png";
+var src = document.getElementById("main-logo-div");
+src.appendChild(img);
+
+
+//document.getElementById("trencin").innerHTML="<span><a href = 'https://www.hockeyarena.net/sk/index.php?p=public_team_info_basic.php&team_id=29244'>Dukla Trencin</a>";
+
+
+var img = document.createElement("img");
+img.src = "img/small/slovan-small.png";
+var src = document.getElementById("small-logo-span");
+var src = document.getElementById("small-logo-slovan");
+src.appendChild(img);
+
+var img = document.createElement("img");
+img.src = "img/small/kosice-small.png";
+var src = document.getElementById("small-logo-span");
+var src = document.getElementById("small-logo-kosice");
+src.appendChild(img);
+
+var img = document.createElement("img");
+img.src = "img/small/trencin-small.png";
+var src = document.getElementById("small-logo-span");
+var src = document.getElementById("small-logo-trencin");
+src.appendChild(img);
+
+var img = document.createElement("img");
+img.src = "img/small/bb-small.png";
+var src = document.getElementById("small-logo-span");
+var src = document.getElementById("small-logo-bb");
+src.appendChild(img);
+
+var img = document.createElement("img");
+img.src = "img/small/poprad-small.png";
+var src = document.getElementById("small-logo-span");
+var src = document.getElementById("small-logo-poprad");
+src.appendChild(img);
+
+var img = document.createElement("img");
+img.src = "img/small/zvolen-small.png";
+var src = document.getElementById("small-logo-span");
+var src = document.getElementById("small-logo-zvolen");
+src.appendChild(img);
+
+var img = document.createElement("img");
+img.src = "img/small/nitra-small.png";
+var src = document.getElementById("small-logo-span");
+var src = document.getElementById("small-logo-nitra");
+src.appendChild(img);
+
+var img = document.createElement("img");
+img.src = "img/small/skalica-small.png";
+var src = document.getElementById("small-logo-span");
+var src = document.getElementById("small-logo-skalica");
+src.appendChild(img);
+
+var img = document.createElement("img");
+img.src = "img/small/zilina-small.png";
+var src = document.getElementById("small-logo-span");
+var src = document.getElementById("small-logo-zilina");
+src.appendChild(img);
+
+var img = document.createElement("img");
+img.src = "img/small/michalovce-small.png";
+var src = document.getElementById("small-logo-span");
+var src = document.getElementById("small-logo-michalovce");
+src.appendChild(img);
+
+var img = document.createElement("img");
+img.src = "img/small/detva-small.png";
+var src = document.getElementById("small-logo-span");
+var src = document.getElementById("small-logo-detva");
+src.appendChild(img);
+
+var img = document.createElement("img");
+img.src = "img/small/mikulas-small.png";
+var src = document.getElementById("small-logo-span");
+var src = document.getElementById("small-logo-mikulas");
+src.appendChild(img);
+
+var img = document.createElement("img");
+img.src = "img/small/snv-small.png";
+var src = document.getElementById("small-logo-span");
+var src = document.getElementById("small-logo-snv");
+src.appendChild(img);
+
+var img = document.createElement("img");
+img.src = "img/small/bardejov-small.png";
+var src = document.getElementById("small-logo-span");
+var src = document.getElementById("small-logo-bardejov");
+src.appendChild(img);
+
